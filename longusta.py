3c28f197cde5a4bd7f4f728a327619c36d112b4a
D34d M8 <spiderierusalim@mail.bg>
2020-11-15 12:28:05 +0300

    Created main file, no coding at all so far

---
+++ longusta.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Longusta (local bongusta??)
+# Let's you save your favorite phlogs on local disk for later offline reading.
+# And preservation! :3
+# 2020, d34dm8|hexed (spiderierusalim@mail.bg)
+# Parsing and some other parts are copied from VF-1 goher client(1)
+# [1](https://github.com/solderpunk/VF-1/)
+
+import urllib.parse
